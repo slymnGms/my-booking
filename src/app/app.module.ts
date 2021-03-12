1f0ef0367b7280b3c86f3c4ebaa675066d5349b6
Sinan <sinan.kandis@ersas.com>
2021-03-12 12:29:32 +0300

    mobil menu leri ve mobil ayarlamaları yapıldı.
    Diğer Çöznürlükler için düzenlemeye devam ediliyor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SearchboxComponent } from './searchbox/searchbox.component';
-import { RoomsComponent } from './rooms/rooms.component';
-import { ExtrasComponent } from './extras/extras.component';
-import { ContactComponent } from './contact/contact.component';
-import { GuestsComponent } from './guests/guests.component';
-import { PaymentComponent } from './payment/payment.component';
-import { BasketComponent } from './basket/basket.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HotelinfoComponent } from './hotelinfo/hotelinfo.component';
-import { HotelphotoComponent } from './hotelphoto/hotelphoto.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSelectModule} from '@angular/material/select';
-import {MatIconModule} from '@angular/material/icon';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import { FacilitysComponent } from './facilitys/facilitys.component';
-
-
-
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SearchboxComponent } from "./searchbox/searchbox.component";
+import { RoomsComponent } from "./rooms/rooms.component";
+import { ExtrasComponent } from "./extras/extras.component";
+import { ContactComponent } from "./contact/contact.component";
+import { GuestsComponent } from "./guests/guests.component";
+import { PaymentComponent } from "./payment/payment.component";
+import { BasketComponent } from "./basket/basket.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HotelinfoComponent } from "./hotelinfo/hotelinfo.component";
+import { HotelphotoComponent } from "./hotelphoto/hotelphoto.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSelectModule } from "@angular/material/select";
+import { MatIconModule } from "@angular/material/icon";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { FacilitysComponent } from "./facilitys/facilitys.component";
+import { MatBadgeModule } from "@angular/material/badge";
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     SearchboxComponent,
     RoomsComponent,
@@ -48,8 +46,8 @@ import { FacilitysComponent } from './facilitys/facilitys.component';
     FooterComponent,
     HotelinfoComponent,
     HotelphotoComponent,
-      FacilitysComponent
-   ],
+    FacilitysComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -66,13 +64,11 @@ import { FacilitysComponent } from './facilitys/facilitys.component';
     MatTooltipModule,
     MatStepperModule,
     MatExpansionModule,
-    MatButtonToggleModule
-    
-
-    
-    
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatSidenavModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,31 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from "@angular/cdk/layout";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+  isMobile: boolean =false;
+  xs: boolean = false;
 
   langs = [
-    {value: 'Turkish', viewValue: 'TR'},
-    {value: 'English', viewValue: 'EN'},
-    {value: 'Russian', viewValue: 'RU'}
+    { value: "Turkish", viewValue: "TR" },
+    { value: "English", viewValue: "EN" },
+    { value: "Russian", viewValue: "RU" },
   ];
 
   currencys = [
-    {value: 'TL', viewValue: 'TL'},
-    {value: 'USD', viewValue: 'USD'},
-    {value: 'EUR', viewValue: 'EUR'},
-    {value: 'GBP', viewValue: 'GBP'}
-
+    { value: "TL", viewValue: "TL" },
+    { value: "USD", viewValue: "USD" },
+    { value: "EUR", viewValue: "EUR" },
+    { value: "GBP", viewValue: "GBP" },
   ];
 
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+      .observe(["(max-width: 959px)", "(max-width: 599px)"])
+      .subscribe((value) => {
+        this.isMobile = value.breakpoints["(max-width: 959px)"];
+        this.xs = value.breakpoints["(max-width: 599px)"];
+      });
   }
 
+  ngOnInit(): void {
+    window.onscroll = function() {
+      if(window.pageYOffset>50) {
+        (<HTMLInputElement>document.querySelector(".sidenav")).style.top="0px";
+      } else {
+        (<HTMLInputElement>document.querySelector(".sidenav")).style.top="50px";
+      }
+      
+    };
+  }
 }
